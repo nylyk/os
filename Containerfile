98c12c03a1312b2e1988088757a0cfa740b2bdc9
Nylyk <nylyk@proton.me>
2025-12-10 02:04:50 +0100

    add base packages

--- Containerfile
+++ Containerfile
@@ -1,6 +1,8 @@
-# Allow build scripts to be referenced without being copied into the final image
-FROM scratch AS ctx
+FROM scratch AS build_files
 COPY build_files /
+
+FROM scratch AS repo_files
+COPY repo_files /
 
 # Base Image
 FROM quay.io/fedora/fedora-bootc:43
@@ -8,11 +10,12 @@ FROM quay.io/fedora/fedora-bootc:43
 # Make /opt immutable and able to be used by the package manager.
 RUN rm -r /opt && mkdir /opt
 
-RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
+RUN --mount=type=bind,from=build_files,source=/,target=/build_files \
+    --mount=type=bind,from=repo_files,source=/,target=/repo_files \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
-    /ctx/build.sh
+    /build_files/build.sh
 
 # Verify final image and contents are correct.
 RUN bootc container lint
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,10 @@
 
 set -ouex pipefail
 
-### Install packages
+# Enable RPM Fusion
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+dnf install -y $(grep -vE '^\s*(#|$)' /repo_files/packages | sort -u)
 
 systemctl enable podman.socket
+systemctl mask systemd-remount-fs.service
